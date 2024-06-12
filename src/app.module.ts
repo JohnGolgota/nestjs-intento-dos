32c180557a8434c7a082f2af1af4d2c3a064e6a8
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-06-12 15:10:56 -0500

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+
+import { HealthModule } from "@src/core/health/health.module";
+
+import { LoggerModule } from "@shared/logger/logger.module";
+
+import { UserModule } from "@contexts/users/user.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    LoggerModule,
+    HealthModule,
+    UserModule,
+  ],
+})
+export class AppModule {}
---
+++ tests/unit/core/health/api/health.controller.test.ts
@@ -0,0 +1,21 @@
+import { Logger } from "@nestjs/common";
+import { createMock, Mock } from "@tests/utils/mock";
+
+import { HealthController } from "@core/health/api/health.controller";
+
+describe("HealthController", () => {
+  let healthController: HealthController;
+  let logger: Mock<Logger>;
+
+  beforeEach(() => {
+    logger = createMock<Logger>();
+    healthController = new HealthController(logger);
+  });
+
+  describe("run", () => {
+    it("should return is healthy", () => {
+      expect(healthController.run()).toEqual({ status: "ok" });
+      expect(logger.log).toHaveBeenCalledTimes(1);
+    });
+  });
+});
